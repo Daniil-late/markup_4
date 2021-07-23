8630b5964c8794df9ee23a55534b96b2cf6f4b4b
Daniil Pozdnyakov <daniil.late@gmail.com>
2021-07-24 00:39:06 +0500

    add project

---
+++ js/pullDownList.js
@@ -0,0 +1,37 @@
+const menuBlock = document.querySelector('.menu')
+const menuHidden = Array.from(menuBlock.querySelectorAll('.menu__hidden'))
+const menuItems = Array.from(menuBlock.getElementsByTagName('p'))
+
+function openMenu(e) {
+    e.stopPropagation()
+    // console.log("Open menu of", this)
+    this.classList.toggle("menu-p-psehdo-rotate")
+    // console.log(this.getAttribute('toggle'))
+    menuHidden.forEach(div => {
+        if ((this.getAttribute('toggle')) === (div.getAttribute('toggler'))) {
+            div.classList.toggle('menu__show')
+        }
+
+    })
+}
+menuItems.forEach(p => {
+    p.addEventListener('mouseup', openMenu)
+})
+
+// document.onclick - function (e) {
+//     console.log(e.target.className)
+// }
+
+const hid = document.querySelector('.menu__hidden')
+
+document.addEventListener('mousedown', (e)=>{
+  if (!e.target.closest('.menu__hidden') && !e.target.classList.contains('menu-p-psehdo-rotate')) {
+    menuHidden.forEach(el =>{
+      el.classList.remove('menu__show')
+    menuItems.forEach(el =>{
+        el.classList.remove('menu-p-psehdo-rotate')
+    })  
+    })
+  }
+  
+})
---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 1,
+      margin: 10,
+      loop: true,
+      nav: false
+    });
+  });
